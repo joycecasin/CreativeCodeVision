a145d4d28e6cdfc14595193e14b3a7051a75adc3
EVVA <58941388+ge-lang@users.noreply.github.com>
2020-07-23 12:06:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-//paralex 
-$(function() {
-    $('.parallax-window').parallax({
-        imageSrc: '../img/1.jpg'
-    });
-    $('.parallax-window-two').parallax({
-        imageSrc: '../img/w.jpg'
-    });
+
+/*mouseparallax*/
+$("#container").mousemove(function(e) {
+    parallaxIt(e, ".slide", -100);
+    parallaxIt(e, "img", -30);
 });
+
+function parallaxIt(e, target, movement) {
+    var $this = $("#container");
+    var relX = e.pageX - $this.offset().left;
+    var relY = e.pageY - $this.offset().top;
+
+    TweenMax.to(target, 1, {
+        x: (relX - $this.width() / 2) / $this.width() * movement,
+        y: (relY - $this.height() / 2) / $this.height() * movement
+    });
+}
+
+/*Team*/
 const team = [{
         name: 'Bert',
         link: ''
